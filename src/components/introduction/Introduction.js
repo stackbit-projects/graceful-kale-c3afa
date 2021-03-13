8fee8ffedbd98353775b23ec6095922c6ac1d82b
vincentregis <vincent.regis1@gmail.com>
2021-03-13 04:17:23 +0000

    Introduction.js: updated Header

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -4,7 +4,7 @@ import Scroll from '../common/Scroll';
 const Introduction = () =>
   <section id="intro" className="wrapper style1 fullscreen fade-up">
     <div className="inner">
-      <h1>Hyperspace</h1>
+      <h1>C'est quoi ton plan de match?</h1>
       <p>
         Just another fine responsive site template designed by{' '}
         <a href="http://html5up.net">HTML5 UP</a>
