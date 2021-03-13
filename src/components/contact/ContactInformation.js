0454857796142c977162e05074b1e05e27c44cf8
Stackbit Code Editor <support@stackbit.com>
2021-03-13 04:55:11 +0000

    ContactInformation.js: changed

--- src/components/contact/ContactInformation.js
+++ src/components/contact/ContactInformation.js
@@ -23,33 +23,6 @@ const ContactInformation = () =>
       </li>
       <li>
         <h3>Social</h3>
-        <ul className="icons">
-          <li>
-            <a href="/#" className="fa-twitter">
-              <span className="label">Twitter</span>
-            </a>
-          </li>
-          <li>
-            <a href="/#" className="fa-facebook">
-              <span className="label">Facebook</span>
-            </a>
-          </li>
-          <li>
-            <a href="/#" className="fa-github">
-              <span className="label">GitHub</span>
-            </a>
-          </li>
-          <li>
-            <a href="/#" className="fa-instagram">
-              <span className="label">Instagram</span>
-            </a>
-          </li>
-          <li>
-            <a href="/#" className="fa-linkedin">
-              <span className="label">LinkedIn</span>
-            </a>
-          </li>
-        </ul>
       </li>
     </ul>
   </section>
