c0ac4528321c69a689c149817534da779d452235
Stackbit Code Editor <support@stackbit.com>
2021-03-13 04:23:58 +0000

    Introduction.js: changed

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -11,7 +11,7 @@ const Introduction = () =>
       <ul className="actions">
         <li>
           <Scroll type="id" element="one">
-            <a href="#one" className="button">
+            <a href="#formulaire" className="button">
               COMMENCER
             </a>
           </Scroll>
