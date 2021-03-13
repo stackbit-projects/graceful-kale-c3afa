4a52ac99ab5d57a08a30c6572f5efb7dd762ff91
Stackbit Code Editor <support@stackbit.com>
2021-03-13 04:44:17 +0000

    ContactForm.js: changed

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -10,7 +10,8 @@ const ContactForm = () =>
           <input type="checkbox" name="message" id="message"/>
             <label for="vehicle1"> I have a bike</label>
           <input type="checkbox" name="message" id="message"/>
-            <label for="vehicle1"> I have a bike</label>          
+            <label for="vehicle1"> I have a bike</label>
+                  
           
         </div>
         <div className="field half">
