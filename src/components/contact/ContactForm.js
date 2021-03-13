142024fb67f29737858157584e4631103b081f35
Stackbit Code Editor <support@stackbit.com>
2021-03-13 04:42:29 +0000

    ContactForm.js: changed

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -6,13 +6,9 @@ const ContactForm = () =>
       <div className="fields">
         <div className="field">
           <label htmlFor="message">Ligues</label>
-          <input type="checkbox" name="message" id="message"/>
-            <label for="vehicle1"> I have a bike</label><br>
-  <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
-  <label for="vehicle2"> I have a car</label><br>
-  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
-  <label for="vehicle3"> I have a boat</label>
-          
+          <input type="checkbox" name="message" id="message"
+            
+          />
           
         </div>
         <div className="field half">
