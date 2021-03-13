62c949b6cfd984c2b50522e336f85fad5a1cc8af
vincentregis <vincent.regis1@gmail.com>
2021-03-13 04:34:46 +0000

    Introduction.js: updated Header

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -4,7 +4,7 @@ import Scroll from '../common/Scroll';
 const Introduction = () =>
   <section id="intro" className="wrapper style1 fullscreen fade-up">
     <div className="inner">
-      <h1>C'est quoi ton plan de match?</h1>
+      <h1>Coach, c'est quoi le plan de match?</h1>
       <p>
         Planifie ton prochain road trip sportif en quelques clics seulement.{' '}
       </p>
