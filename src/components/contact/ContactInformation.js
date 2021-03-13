f3bbde3b2fb1b708a42dc485b1f44c0abf50c36d
vincentregis <vincent.regis1@gmail.com>
2021-03-13 04:54:06 +0000

    ContactInformation.js: updated Header in List Item 2

--- src/components/contact/ContactInformation.js
+++ src/components/contact/ContactInformation.js
@@ -14,7 +14,7 @@ const ContactInformation = () =>
         </span>
       </li>
       <li>
-        <h3>Choisis les événements qui t'intéressent</h3>
+        <h3>2. Choisis les événements qui t'intéressent</h3>
         <a href="/#">user@untitled.tld</a>
       </li>
       <li>
