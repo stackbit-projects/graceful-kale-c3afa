4d62f1a8366f5d704ef1dc35e2917655d05f51a7
Stackbit Code Editor <support@stackbit.com>
2021-03-13 04:26:19 +0000

    Introduction.js: changed

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -7,10 +7,6 @@ const Introduction = () =>
       <h1>C'est quoi ton plan de match?</h1>
       <p>
         Planifie ton prochain road trip sportif en quelques clics seulement. Choisis tes dates, tes sports et la distance que t'es prêt à voyager, et on s'occupe du reste.{' '}
-        <a href="http://html5up.net">HTML5 UP</a>
-        <br />
-        and released for free under the{' '}
-        <a href="http://html5up.net/license">Creative Commons</a>.
       </p>
       <ul className="actions">
         <li>
