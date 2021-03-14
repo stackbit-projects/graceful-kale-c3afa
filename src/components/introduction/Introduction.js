4c3db039aa0cc24315971db2ccf78a8b95df77b7
Stackbit Code Editor <support@stackbit.com>
2021-03-14 03:07:10 +0000

    Introduction.js: changed

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -17,6 +17,7 @@ const Introduction = () =>
           </Scroll>
         </li>
         <li>
+          <Scroll type="id" element="three">
             <a href="#three" className="button">
               RÉCUPÉRER UN PLAN DE MATCH
             </a>
